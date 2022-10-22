57876b822947a4e81d45463eaf8e94e33d255c0f
Konstantin Obenland <obenland@gmx.de>
2022-10-22 16:45:27 -0700

    First pass

---
+++ tests/test-environment.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Environment test file.
+ *
+ * @package wp-approve-user
+ */
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * These tests prove phpunit test setup works.
+ */
+class EnvironmentTest extends TestCase {
+
+
+	/**
+	 * This tests makes sure:
+	 *
+	 * - WordPress functions are defined
+	 * - WordPress database can be written to.
+	 */
+	public function test_wordpress() {
+		global $wpdb;
+
+		$this->assertTrue( is_object( $wpdb ) );
+		$id = wp_insert_post(
+			array(
+				'post_type'    => 'post',
+				'post_title'   => 'roy',
+				'post_content' => 'sivan',
+			)
+		);
+		$this->assertTrue( is_numeric( $id ) );
+	}
+}
