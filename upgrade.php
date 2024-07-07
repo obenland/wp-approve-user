7696427825005536dc9438365dbc7e5a9d41f3a8
Konstantin Obenland <obenland@gmx.de>
2024-07-06 19:51:29 -0500

    Create an upgrade routine for meta value (#32)

---
+++ upgrade.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Upgrade routines.
+ *
+ * @package WP Approve User
+ */
+
+/**
+ * Upgrade routine.
+ */
+function wpau_upgrade_all() {
+	global $wpau_db_version;
+
+	$wpau_current_db_version = get_site_option( 'wpau_db_version', 0 );
+
+	if ( $wpau_current_db_version === $wpau_db_version ) {
+		return;
+	}
+
+	if ( $wpau_current_db_version < 12 ) {
+		wpau_upgrade_to_12();
+	}
+
+	update_site_option( 'wpau_db_version', $wpau_db_version );
+}
+add_action( 'admin_init', 'wpau_upgrade_all' );
+
+/**
+ * Updates all user meta values from true to 'approved'.
+ */
+function wpau_upgrade_to_12() {
+	global $wpdb;
+
+	// phpcs:disable WordPress.DB
+	$wpdb->update(
+		$wpdb->usermeta,
+		array( 'meta_value' => 'approved' ),
+		array(
+			'meta_key'   => 'wp-approve-user',
+			'meta_value' => true,
+		)
+	);
+	// phpcs:enable WordPress.DB
+}
