c3110f1b089f41f3e107baab8c2a559ec7ea14cd
Konstantin Obenland <obenland@gmx.de>
2024-10-26 20:43:43 -0500

    Set any pending users to pending

--- upgrade.php
+++ upgrade.php
@@ -41,4 +41,40 @@ function wpau_upgrade_to_12() {
 		)
 	);
 	// phpcs:enable WordPress.DB
+
+	wpau_set_users_pending();
 }
+
+/**
+ * Set 100 users to pending per cron run.
+ *
+ * @param int $processed Number of users processed.
+ */
+function wpau_set_users_pending( $processed = 0 ) {
+	$users = get_users(
+		array(
+			'fields'     => 'ID',
+			'number'     => 100,
+			'offset'     => $processed,
+			'meta_query' => array( // phpcs:ignore WordPress.DB.SlowDBQuery.slow_db_query_meta_query
+				array(
+					'key'     => 'wp-approve-user',
+					'compare' => 'NOT EXISTS',
+				),
+			),
+		)
+	);
+	$users = array_diff( $users, array( get_user_by( 'email', get_bloginfo( 'admin_email' ) )->ID ) );
+
+	foreach ( $users as $user_id ) {
+		update_user_meta( $user_id, 'wp-approve-user', 'pending' );
+	}
+
+	$processed += count( $users );
+	$count      = count_users();
+
+	if ( $processed < $count['total_users'] ) {
+		wp_schedule_single_event( time() + 5, 'wpau_pending_users_cron', array( $processed ) );
+	}
+}
+add_action( 'wpau_pending_users_cron', 'wpau_set_users_pending' );
